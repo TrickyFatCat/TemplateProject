356b08a5cee0a46c83a4b6c8efa18964735658d7
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-09-04 23:07:00 +0300

    Added player deactivation method

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -4,9 +4,11 @@ class_name Player
 export(int) var hitpoints_max := 100
 
 var is_invulnerable : bool = false
+var is_active : bool = true setget set_is_active
 
 onready var hitPoints : HitPoints = $HitPoints
 onready var damageDetector : DamageDetector = $DamageDetector
+onready var stateMachine : StateMachine = $StateMachine
 
 
 func _on_DamageDetector_get_damage(damage: int) -> void:
@@ -21,6 +23,7 @@ func _on_HitPoints_value_decreased() -> void:
 
 func _on_HitPoints_value_zero() -> void:
 	Events.emit_signal("player_dead")
+	self.is_active = false
 
 
 func _init() -> void:
@@ -30,3 +33,10 @@ func _init() -> void:
 func _ready() -> void:
 	hitPoints.value_max = hitpoints_max
 	pass
+
+
+func set_is_active(value: bool) -> void:
+	is_active = value 
+	damageDetector.monitoring = value
+	damageDetector.monitorable = value
+	stateMachine.set_process_unhandled_input(value)
