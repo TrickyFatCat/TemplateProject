fcd82f119906777c8ab14ac80d91cc8f4ac476fc
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-09-09 21:46:22 +0300

    Added weapon switching on mouse wheel

--- src/Player/WeaponController/PlayerWeaponController.gd
+++ src/Player/WeaponController/PlayerWeaponController.gd
@@ -1,14 +1,55 @@
 extends WeaponController
 
-onready var active_weapon : Weapon = $BaseWeapon
+const WEAPONS := [
+	"res://content/Weapons/TestWeapon0.tres",
+	"res://content/Weapons/TestWeapon1.tres"
+]
+
+var weapon_id : int = 0
+
+onready var weapon : Weapon = $BaseWeapon
+
+
+func _ready() -> void:
+	_switch_weapon(3)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
-		active_weapon.process_shoot()
+		weapon.process_shoot()
+
+	_process_weapon_switch_wheel(event) 
 
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	flip_weapon()
 	switch_z_index()
+
+
+func _switch_weapon(new_weapon_id: int) -> void:
+	if new_weapon_id < 0 or new_weapon_id > WEAPONS.size() - 1:
+		var error_text := "A weapon with id %s hasn't been found." % new_weapon_id
+		push_error(error_text)
+		return
+	
+	var weapon_data = load(WEAPONS[new_weapon_id])
+	weapon.apply_parameters(weapon_data)
+
+
+func _process_weapon_switch_wheel(event: InputEvent) -> void:
+	if event.is_action_pressed("choose_weapon_next"):
+		weapon_id += 1
+		
+		if weapon_id == WEAPONS.size():
+			weapon_id = 0
+		
+		_switch_weapon(weapon_id)
+	
+	if event.is_action_pressed("choose_weapon_previous"):
+		weapon_id -= 1
+
+		if weapon_id < 0:
+			weapon_id = WEAPONS.size() - 1
+		
+		_switch_weapon(weapon_id)
--- src/main/BaseWeapon/BaseWeapon.gd
+++ src/main/BaseWeapon/BaseWeapon.gd
@@ -9,10 +9,16 @@ const RECOVERY_SPEED : float = 0.2
 var weapon_name : String
 var mode : int
 var bullet_type : int
+var projectile_recourse : String
 var damage : int
 var rate_of_fire : float
 var bullets_count : int
+var is_spread_dynamic : bool
+var spread_min : float
+var spread_max : float
 var spread : float
+var spread_increase_shift : float
+var spread_decrease_shift : float
 
 onready var sprite : Sprite = $Sprite
 onready var sprite_init_pos : Vector2 = sprite.position
@@ -27,6 +33,23 @@ func process_shoot() -> void:
     _apply_recoil()
     emit_signal("shoot")
     pass
+
+
+func apply_parameters(parameters: WeaponParameters) -> void:
+    weapon_name = parameters.weapon_name
+    sprite.texture = parameters.sprite
+    mode = parameters.mode
+    bullet_type = parameters.bullet_type
+    projectile_recourse = parameters.projectile_recourse
+    damage = parameters.damage
+    rate_of_fire = parameters.rate_of_fire
+    bullets_count = parameters.bullets_count
+    is_spread_dynamic = parameters.is_spread_dynamic
+    spread_min = parameters.spread_min
+    spread_max = parameters.spread_max
+    spread = spread_min
+    spread_increase_shift = parameters.spread_increase_shift
+    spread_decrease_shift = parameters.spread_decrease_shift
 
 
 func _apply_recoil() -> void:
