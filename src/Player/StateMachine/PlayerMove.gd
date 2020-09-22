2977ce8417b58ad2bbf84fa31339ed46be5a2c54
Artyom Volkov <tricky.fat.cat@gmail.com>
2020-09-22 15:04:49 +0300

    Player movement update

--- src/Player/StateMachine/PlayerMove.gd
+++ src/Player/StateMachine/PlayerMove.gd
@@ -1,10 +1,12 @@
 extends State
 
-var velocity : float = 0.0
-var velocity_max : float = 500.0
-var acceleration : float = 100.0
-var friction : float = 100.0
-var motion : Vector2 = Vector2.ZERO
+# TODO make base movement state for all entities
+# TODO make these parameter applyable from the owner
+var move_direction : Vector2 = Vector2.ZERO
+var velocity : Vector2 = Vector2.ZERO
+var velocity_max : float = 400.0
+var acceleration : float = 2500.0
+var friction : float = 2500.0
 
 onready var sprite : AnimatedSprite = get_parent().get_parent().get_node("AnimatedSprite")
 
@@ -18,8 +20,10 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	motion = calculate_motion(delta, calculate_move_direction())
-	owner.move_and_slide(motion)
+	_calculate_move_direction()
+	calculate_velocity_y(delta, move_direction)
+	calculate_velocity_x(delta, move_direction)
+	velocity = owner.move_and_slide(velocity)
 
 
 func enter(msg: Dictionary = {}) -> void:
@@ -30,22 +34,35 @@ func exit() -> void:
 	return
 
 
-func calculate_move_direction() -> Vector2:
-	var direction := Vector2.ZERO
-	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	return direction.normalized() if stateMachine.is_processing_unhandled_input() else Vector2.ZERO
+func _calculate_move_direction() -> void:
+	move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	move_direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	move_direction.normalized()
 
 
-func calculate_motion(delta: float, direction: Vector2) -> Vector2:
-	if direction != Vector2.ZERO and velocity <= velocity_max:
-		velocity += acceleration * delta
-		velocity = min(velocity, velocity_max)
-	elif velocity != 0 or velocity > velocity_max:
-		velocity -= friction * delta
-		velocity = max(velocity, 0)
-	
-	if direction == Vector2.ZERO and velocity > 0:
-		direction = motion.normalized() * -1
-	
-	return velocity * direction
+func calculate_velocity_x(delta: float, direction: Vector2) -> void:
+	if direction.x != 0 and abs(velocity.x) <= velocity_max:
+		velocity.x += acceleration * direction.x * delta
+		velocity.x = clamp(velocity.x, -velocity_max, velocity_max)
+	elif velocity.x != 0 or abs(velocity.x) > velocity_max:
+		var friction_direction = -sign(velocity.x)
+		velocity.x += friction * friction_direction * delta
+		
+		if friction_direction < 0:
+			velocity.x = max(velocity.x, 0)
+		elif friction_direction > 0:
+			velocity.x = min(velocity.x, 0)
+
+
+func calculate_velocity_y(delta: float, direction: Vector2) -> void:
+	if direction.y != 0 and abs(velocity.y) <= velocity_max:
+		velocity.y += acceleration * direction.y * delta
+		velocity.y = clamp(velocity.y, -velocity_max, velocity_max)
+	elif velocity.y != 0 or abs(velocity.y) > velocity_max:
+		var friction_direction = -sign(velocity.y)
+		velocity.y += friction * friction_direction * delta
+		
+		if friction_direction < 0:
+			velocity.y = max(velocity.y, 0)
+		elif friction_direction > 0:
+			velocity.y = min(velocity.y, 0)
